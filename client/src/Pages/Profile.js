6274f1628345298f002db1345a1dd364ceb34391
SAC-21 <sachingupta26012002@gmail.com>
2021-11-25 20:21:12 +0530

    Profile page updated

--- client/src/Pages/Profile.js
+++ client/src/Pages/Profile.js
@@ -1,46 +1,97 @@
-import { React, useEffect, useState } from 'react';
-import { Image } from 'react-bootstrap';
-import axios from 'axios';
-
+import { React, useEffect, useState } from "react";
+import { Image } from "react-bootstrap";
+import axios from "axios";
+import "./Profile.css";
+import "../index.css";
 const Profile = () => {
+  const [details, setDetails] = useState();
 
-    const [details, setDetails] = useState();
+  useEffect(() => {
+    const url = "http://localhost:8080/profile";
+    axios({
+      method: "POST",
+      withCredentials: true,
+      url: url,
+      data: {
+        email: localStorage.getItem("email"),
+      },
+    }).then((res) => {
+      console.log(res.data);
+    });
+  }, []);
 
-    useEffect(() => {
-        const url =
-            "http://localhost:8080/profile";
-        axios({
-            method: "POST",
-            withCredentials: true,
-            url: url,
-            data: {
-                email: localStorage.getItem('email')
-            }
-        }).then((res) => {
-            console.log(res.data);
-        });
-    }, []);
-
-    return (
-        <div style={{ textAlign: "center" }}>
-            <div style={{
+  return (
+    <>
+      <div style={{ display: "flex", justifyContent: "center" }}>
+        {/* <div style={{
                 borderBottomLeftRadius: "90%", borderBottomRightRadius: "90%", 
                 height: "100px", backgroundColor: "#8d9496", display: "flex", justifyContent: "center"
-            }}>
-                <Image style={{ height: "150px", width: "175px", margin: "30px" }} src="https://www.grunge.com/img/gallery/the-tragic-real-life-story-of-shirley-temple/l-intro-1603459690.jpg" roundedCircle />
-            </div>
-            <div style={{ marginTop: "90px" }}>
-                <p style={{ fontSize: "2rem", fontWeight: "600", margin: "0" }} >{localStorage.getItem("name")}</p>
-                <p style={{ fontSize: "1.2rem", fontWeight: "400", margin: "0" }} >{localStorage.getItem("email")}</p>
-                <p style={{ fontSize: "1.2rem", fontWeight: "400", margin: "0" }} >{localStorage.getItem("contactNumber")}</p>
-            </div>
-            <div style={{height:"35vh"}}></div>
-            <div style={{
-                borderTopLeftRadius: "90%", borderTopRightRadius: "90%", 
-                height: "90px", backgroundColor: "#8d9496", display: "flex", justifyContent: "center"
-            }}></div>
+            }}> */}
+        <Image
+          style={{
+            position: "absolute",
+            height: "239px",
+            width: "239px",
+            marginTop: "200px",
+          }}
+          src="https://www.whatsappprofiledpimages.com/wp-content/uploads/2021/08/Profile-Photo-Wallpaper.jpg"
+          roundedCircle
+        />
+        
+        <div
+          style={{
+            margin: "140px 0 0 700px",
+          }}
+        >
+          <div
+            style={{
+              fontFamily: "'Josefin Sans', sans-serif",
+              marginBottom: "20px",
+              height: "50px",
+              fontSize: "2rem",
+              fontWeight: "400",
+            }}
+          >
+            Details
+          </div>
+          <div className="details">Name:</div>
+          <div
+            style={{
+              fontSize: "1.1rem",
+              fontWeight: "450",
+              fontFamily: "'Open Sans', sans-serif",
+              marginRight: "50px",
+              height: "50px",
+            }}
+          >
+            {localStorage.getItem("name")}
+          </div>
+          <div className="details">Email:</div>
+          <div
+            style={{
+              fontSize: "1.1rem",
+              fontWeight: "450",
+              fontFamily: "'Open Sans', sans-serif",
+              height: "50px",
+            }}
+          >
+            {localStorage.getItem("email")}
+          </div>
+          <div className="details">Contact Number</div>
+          <div
+            style={{
+              fontSize: "1.1rem",
+              fontWeight: "450",
+              fontFamily: "'Open Sans', sans-serif",
+              height: "50px",
+            }}
+          >
+            {localStorage.getItem("contactNumber")}
+          </div>
         </div>
-    )
-}
+      </div>
+    </>
+  );
+};
 
 export default Profile;
